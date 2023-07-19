7f112847d2543b940d47d02454eeb372c932eb0d
Gokulsenthil <gokulsenthil899@gmail.com>
2023-07-19 22:40:57 +0530

    dice commit

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,29 +1,31 @@
-
-
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
-  
-const startAligment =  Alignment.topLeft;
+
+const startAligment = Alignment.topLeft;
 const endAligment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer(this.color1, this.color2,{super.key});
-     
-    const GradientContainer.purple({super.key}) :color1 = Colors.deepPurple, color2 = Colors.indigo;
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = const Color.fromARGB(255, 124, 78, 203),
+        color2 = const Color.fromARGB(255, 5, 20, 107);
 
   final Color color1;
   final Color color2;
+  
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration:  BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1,color2],
-          begin:  startAligment,
-          end: endAligment ,
+          colors: [color1, color2],
+          begin: startAligment,
+          end: endAligment,
         ),
       ),
       child:  Center(
-        child: Image.asset('assets/dice-images/dice-2.png'),
+        child: DiceRoller(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(
-   const MaterialApp(
+    const  MaterialApp(
       home: Scaffold(
-        body: GradientContainer.purple(),
+        // ignore: unnecessary_const
+        body: GradientContainer( Color.fromARGB(255, 2, 18, 73),
+            Color.fromARGB(255, 7, 25, 128),),
       ),
     ),
   );
 }
-
